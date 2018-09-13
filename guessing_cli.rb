12add85456e51e69925ddfc31b495201c97420b0
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-13 05:29:27 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,11 +5,13 @@ def run_guessing_game
   loop do
     guess = gets.chomp
     return puts "Goodbye!" if guess == "exit"
-    return puts "You guessed the correct number!" if guess.to_i == number
+    puts "You guessed the correct number!" if guess.to_i == number
+    correct = guess.to_i == number
   
     comp = rand(1..6)
     puts "The computer guessed #{comp}."
-    if comp == number
+    correct ||= comp == number
+    if correct
       puts "It was correct! Picking a new random number."
       number = rand(1..6)
     end
